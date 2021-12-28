6c78f4ccd6d6edcd1f23210ee8c1d96e71098f9f
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2021-12-28 22:53:03 +0900

    update

---
+++ functions/strings.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+#
+# Comment, .env, file utils
+
+#######################################
+# Delete some line in bash style comment # started.
+# Globals:
+#   None
+# Arguments:
+#   Stdout to delete comment ,a path. this param 
+# Outputs:
+#   Writes not stared bash style comment # line to stdout
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   cat file | eraze_comment_line
+#   eraze_comment_line file
+#######################################
+function eraze_comment_line {
+
+  # you use Darwin, set alias for use Gnu command.
+  if ! gnu_alias; then
+      return 1;
+  fi
+  # unalias gnu command
+  trap 'gnu_unalias' RETURN
+
+  local filepath=$1
+  # if you ,use pipe line value.
+  if [ -z $filepath ]; then
+      filepath=-
+  fi
+
+  cat $filepath | sed 's/# .*$//'
+}
+
+#######################################
+# Delete some line in bash style comment # started.
+# Globals:
+#   None
+# Arguments:
+#   Stdout to delete comment ,a path. this param 
+# Outputs:
+#   export VARIABLE_NAME=VARIABLE list
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   cat file | convert_export_line
+#   convert_export_line file
+#######################################
+function convert_export_line {
+  # you use Darwin, set alias for use Gnu command.
+  if ! gnu_alias; then
+      return 1;
+  fi
+  # unalias gnu command
+  trap 'gnu_unalias' RETURN
+
+  local filepath=$1
+  # if you ,use pipe line value.
+  if [ -z $filepath ]; then
+      filepath=-
+  fi
+  # eraze_comment_line | xargs -I {} echo export {}
+  eraze_comment_line $filepath | xargs -I {} echo export {}
+}
+
+#######################################
+# load .env file. set your environment variable.
+# Globals:
+#   None
+# Arguments:
+#   load csv file ,a path.
+# Outputs:
+#   None
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   load_env .env
+#######################################
+function load_env {
+
+  local filepath=$1
+  # if you ,use pipe line value.
+  if [ -z $filepath ]; then
+      echo 'you send parameter.' >&2
+      return 1
+  fi
+  # eval "$(cat $1 | sed 's/# .*$//' | xargs -I {} echo export {};)"
+  eval "$(cat $filepath | convert_export_line;)"
+}
+
+#######################################
+# convert text to windows utf8BOM
+# Globals:
+#   None
+# Arguments:
+#   filepath
+# Outputs:
+#   None
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   convert_text_windows_style windows_style.csv
+#######################################
+function convert_text_windows_style {
+  local filepath=$1
+  # check nkf command is exists
+  if ! type "nkf" > /dev/null; then
+      echo "nkf is not exists." >&2
+      return 1
+  fi
+  # if $filepath not exists, return function.
+  if [ -z $filepath ]; then
+      echo "${filepath} is not exists." >&2
+      return 1
+  fi
+  # change to utf8(BOM) CRLF
+  nkf --overwrite --oc=UTF-8-BOM -Lw $filepath
+}
+
+#######################################
+# cat 
+# Globals:
+#   None
+# Arguments:
+#   Stdout to delete comment ,a path. this param 
+# Outputs:
+#   Writes lines delete file_header.
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   cat filelist | cat_map_convert_text_windows_style
+#   cat_map_convert_text_windows_style file1 file2 file3 ...
+#######################################
+function cat_map_convert_text_windows_style {
+  local filelist=$@
+  if [ -z $filelist ]; then
+      filelist=`cat -`
+      #  if filelist 
+      if [ -z $filelist ]; then
+          return 1
+      fi
+  fi
+  # check file exists
+  if file_exists $filelist; then
+      return 1
+  fi
+  
+  # map function
+  cat_map convert_text_windows_style $filelist
+}
+
---
+++ functions/util.sh
@@ -0,0 +1,246 @@
+#!/bin/bash
+#
+# utils, often use.
+
+#######################################
+# write stderr syntax. google style(https://google.github.io/styleguide/shellguide.html)
+# Globals:
+#   None
+# Arguments:
+#   All.
+# Outputs:
+#   None
+# Returns:
+#   0 if thing was set alias, non-zero on error.
+# Example:
+#   err "Unable to do_something"  # => you use use sed
+#######################################
+function err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+#######################################
+# Delete file_header for bash_script. shebang and below 2 line
+# Globals:
+#   None
+# Arguments:
+#   Stdout to delete comment ,a path. this param 
+# Outputs:
+#   Writes lines delete file_header.
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   cat file | delete_file_header
+#   delete_file_header file
+#######################################
+function delete_file_header {
+  # you use Darwin, set alias for use Gnu command.
+  if ! gnu_alias; then
+    return 1;
+  fi
+  # unalias gnu command
+  trap 'gnu_unalias' RETURN
+
+  local filepath=$1
+  # if you ,use pipe line value.
+  if [ -z $filepath ]; then
+    filepath=-
+  fi
+
+  # 最初の二行飛ばす。
+  cat $filepath | sed '1,2d'
+}
+
+#######################################
+# cat 
+# Globals:
+#   None
+# Arguments:
+#   Stdout to delete comment ,a path. this param 
+# Outputs:
+#   Writes lines delete file_header.
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   cat file | delete_file_header
+#   delete_file_header file
+#######################################
+function cat_map_delete_file_header {
+  # set gnu alias.
+  if ! gnu_alias; then
+    return 1;
+  fi
+  # unalias gnu command
+  trap 'gnu_unalias' RETURN
+  local filelist=$@
+  if [ -z $filelist ]; then
+    filelist=`cat -`
+  fi
+  # check file exists
+  if file_exists $filelist; then
+    return 1
+  fi
+  # map function
+  cat_map delete_file_header $filelist
+}
+
+#######################################
+# cat 
+# Globals:
+#   None
+# Arguments:
+#   Stdout to delete comment ,a path. this param 
+# Outputs:
+#   Writes lines delete file_header.
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   cat file_list | cat_map # file_list 
+#   cat_map function file1 file2 ...
+#######################################
+function cat_map {
+  local mapF=$1
+  local filelist=${@:2:($#-1)}
+  # if not exists function cause error.
+  if [ -z $1 ]; then
+    echo 'pass function name.' >&2
+    return 1
+  fi
+  # check exists function
+  if ! type -t $mapF | grep "function" > /dev/null; then
+    return 1
+  fi
+
+  # if you ,use pipe line value.
+  if [ -z $filelist ]; then
+    filelist=`cat -`
+  fi
+  # check file exists
+  if file_exists $filelist; then
+    return 1
+  fi
+
+  for filepath in $filelist; do
+    # execute function
+    $mapF $filepath
+  done
+}
+
+#######################################
+# cat 
+# Globals:
+#   None
+# Arguments:
+#   Stdout to delete comment ,a path. this param 
+# Outputs:
+#   Writes lines delete file_header.
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   cat file | delete_file_header
+#   delete_file_header file
+#######################################
+function file_exists {
+
+  for filepath in $@; do
+    if ! [ -e $filepath ]; then
+      echo "${filepath} is not exists." >&2
+      return 1
+    fi
+  done
+}
+
+#######################################
+# check exists gnu command alias gnu command for Mac.
+# gsed, gawk, gxargs,... and many
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+# Returns:
+#   0 if thing was set alias, non-zero on error.
+# Example:
+#   gnu_exists # => you use use sed
+#######################################
+function gnu_exists {
+  # you use Darwin, set alias for use Gnu sed, awk, xargs
+  if uname | grep -e Darwin -e BSD > /dev/null; then
+    # gsedがなかったら、return 1
+    if ! type "gsed" > /dev/null; then
+      echo "gsed is not exists." >&2
+      return 1
+    fi
+    if ! type "gawk" > /dev/null; then
+      echo "gawk is not exists." >&2
+      return 1
+    fi
+    if ! type "gxargs" > /dev/null; then
+      echo "gxargs is not exists." >&2
+      return 1
+    fi
+    if ! type "gfind" > /dev/null; then
+      echo "gfind is not exists." >&2
+      return 1
+    fi
+  fi
+}
+
+#######################################
+# set alias gnu command for Mac.
+# Globals:
+#   GNU_ALIAS
+# Arguments:
+#   None
+# Outputs:
+#   None
+# Returns:
+#   0 if thing was set alias, non-zero on error.
+# Example:
+#   gnu_alias # => you use use sed
+#######################################
+function gnu_alias {
+  # include gurad.
+  # if you set alias, return.
+  if [ -n $GNU_ALIAS ]; then
+    return
+  fi
+  # you use Darwin, set alias for use Gnu sed, awk, xargs
+  if uname | grep -e Darwin -e BSD > /dev/null; then
+    if ! gnu_exists; then
+      echo "Any Gnu command is not exists." >&2
+      return 1
+    fi
+    alias sed=gsed
+    alias awk=gawk
+    alias xargs=gxargs
+    alias find=gfind
+    # set variable for 
+    GNU_ALIAS=GNU_ALIAS
+  fi
+}
+
+#######################################
+# unalias gnu command for Mac.
+# Globals:
+#   GNU_ALIAS
+# Arguments:
+#   None
+# Outputs:
+#   None
+# Returns:
+#   0 if thing was deleted, non-zero on error.
+# Example:
+#   gnu_unalias # => you use use sed
+#######################################
+function gnu_unalias {
+  # you use Darwin, set alias for use Gnu sed, awk, xargs
+  if uname | grep -e Darwin -e BSD > /dev/null; then
+    unalias sed
+    unalias awk
+    unalias xargs
+    unalias find
+    unset GNU_ALIAS
+  fi
+}
